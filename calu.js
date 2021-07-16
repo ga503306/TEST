b9c254fd44a9f8c963c9916bef20716b5e007dd6
hzt <ga203306@yahoo.com.tw>
2021-07-16 20:53:00 +0800

    Add files via upload

---
+++ calu.js
@@ -0,0 +1,36 @@
+﻿var Calu = {
+    init: function () {
+        Calu.getCurrect();
+    },
+    getCurrect: function () {
+        $.get("https://raw.githubusercontent.com/ga503306/TEST/main/TEST", function (data) {
+            var config = JSON.parse(data);
+            console.log(config);
+            price_low = config.price_low;
+            price_high = config.price_high;
+            onsale = config.onsale;
+            turner = config.turner;
+        });
+    },
+    check: function () {
+        var width = $('#width').val();
+        var height = $('#height').val();
+        //進位到整數 = 幅數 = 寬度2（兩倍用布量）/30.3（換算成尺）/5（5呎，一幅是五尺）=
+        var vu = Math.ceil(width * 2 / 30.3 / 5);
+        //進位到第一位數 = 碼數 =（高度/30.3）+1（上下反摺30公分左右）｝幅數/3（一碼3尺）
+        var ma = Math.ceil((((height / 30.3) + 1) * vu / 3) / 10) * 10;
+        //布價 = 折數 * 單價 * 碼數
+        //最高價 最低價
+        var amt_low = onsale * price_low * ma;
+        var amt_high = onsale * price_high * ma;
+        $('#amt_low').val(amt_low);
+        $('#amt_high').val(amt_high);
+        //軌道 (寬度/30.3)*100=軌道前
+        var rail = roundToTwo(width / 30.3) * turner
+        $('#rail').val(rail);
+    }
+}
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2") + "e-2");
+}
